4c375e2f7fdb40146c05e0128b3185cfa1194e22
Ngonyoku <ngonyoku001@gmail.com>
2020-07-21 15:16:33 +0300

    Creating an Inner Class that derives from Runnable Interface.

--- app/src/main/java/com/rickprojects/threading/MainActivity.java
+++ app/src/main/java/com/rickprojects/threading/MainActivity.java
@@ -19,10 +19,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startThread(View view) {
-        ExampleThread thread = new ExampleThread(10);
-        thread.start();
+//        new ExampleThread(10).start(); /*------Method 1------*/
+        new Thread(new ExammpleRunnable(10)).start(); /*------Method 2------*/
     }
 
+    /**
+     * ------Method 1------
+     * We can create an Inner class that Extends from the Thread Class and override the
+     * run() method. We the do all the work on the run() method.
+     */
     class ExampleThread extends Thread {
         int seconds;
 
@@ -33,6 +38,31 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             for (int i = 0; i < 10; i++) {
+                Log.d(TAG, "startThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * ------Method 2------
+     * We can also create an Inner class which will Implement the Runnable Interface and override
+     * the run() method. All the Work is done on the overriden run() method
+     */
+    class ExammpleRunnable implements Runnable {
+        int seconds;
+
+        public ExammpleRunnable(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < seconds; i++) {
                 Log.d(TAG, "startThread: " + i);
                 try {
                     Thread.sleep(1000);
