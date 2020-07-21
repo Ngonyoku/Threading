2a82219ad95f9cdaabfb2c49c4dc38213b0676cf
Ngonyoku <ngonyoku001@gmail.com>
2020-07-21 15:24:58 +0300

    Creating an Inner Class that derives from Runnable Interface.

--- app/src/main/java/com/rickprojects/threading/MainActivity.java
+++ app/src/main/java/com/rickprojects/threading/MainActivity.java
@@ -27,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
      * ------Method 1------
      * We can create an Inner class that Extends from the Thread Class and override the
      * run() method. We the do all the work on the run() method.
+     * To execute this code, we will create an instance of this class and call the the start()
+     * method. e.g
+     * [new ExampleThread(10).start();]
      */
     class ExampleThread extends Thread {
         int seconds;
@@ -51,7 +54,11 @@ public class MainActivity extends AppCompatActivity {
     /**
      * ------Method 2------
      * We can also create an Inner class which will Implement the Runnable Interface and override
-     * the run() method. All the Work is done on the overriden run() method
+     * the run() method. All the Work is done on the overriden run() method. This is the preferred
+     * approach. To execute this on a Thread, we wil need to pass an object of this class (e.g
+     * ExampleRunnable Class) as a parameter to an object of the Thread class. we then call the
+     * start() method on from the Thread object. e.g
+     * [new Thread(new ExammpleRunnable(10)).start();]
      */
     class ExammpleRunnable implements Runnable {
         int seconds;
