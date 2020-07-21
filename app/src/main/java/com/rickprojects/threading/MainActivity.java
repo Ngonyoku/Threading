ce59d818af3433c98bfca4e3d7e0245edcea75b5
Ngonyoku <ngonyoku001@gmail.com>
2020-07-21 14:46:44 +0300

    Creating an Inner Class that derives from Thread class.

--- app/src/main/java/com/rickprojects/threading/MainActivity.java
+++ app/src/main/java/com/rickprojects/threading/MainActivity.java
@@ -3,12 +3,43 @@ package com.rickprojects.threading;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void stopThread(View view) {
+    }
+
+    public void startThread(View view) {
+        ExampleThread thread = new ExampleThread(10);
+        thread.start();
+    }
+
+    class ExampleThread extends Thread {
+        int seconds;
+
+        ExampleThread(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                Log.d(TAG, "startThread: " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
